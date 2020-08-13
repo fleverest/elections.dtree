3a99ce44826867c022254a1565a4372179329964
fleverest <41366210+fleverest@users.noreply.github.com>
2020-08-13 17:02:02 +1000

    added IRV tree and simulation

---
+++ R/irvElection/irvSimulate.R
@@ -0,0 +1,36 @@
+# this function takes an IRV election tree and a number of ballots as arguments
+# and simulates that number of ballots, currently uniformly.
+irvSimulate <- function(
+    root, # the election root node
+    n = 1000 # number of ballots to simulate on tree
+){
+
+    #ensure extraDistr is available
+    if(!require(extraDistr)) {
+        stop("this function requires the extraDistr package")
+    }
+
+    # randomLeaf function takes a node arg and selects a random leaf below it
+    randomLeaf <- function(node) {
+        height <- node$height - 1
+        # Specifies a random path (by child indices)
+        path <- mapply(function(x) rdunif(1,1,x), height:1)
+
+        # traverses tree to the leaf
+        for( num in path ){
+            node <- node$children[[num]]
+        }
+
+        # returns the leaf
+        return(node)
+    }
+
+
+    # Repeat n times
+    for( i in 1:n ) {
+
+        leaf <- randomLeaf(root)
+        leaf$ballots = leaf$ballots + 1
+
+    }
+}
