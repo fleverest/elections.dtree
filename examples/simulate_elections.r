7808e930a03225f3fac0eafbfc6426fdd791f9bf
Floyd Everest <me@floydeverest.com>
2021-04-29 16:25:51 +1000

    parallelized!

--- examples/simulate_elections.r
+++ examples/simulate_elections.r
@@ -1,52 +1,37 @@
 require('dirtree.elections')
 require('readr')
+if (!is.loaded("mpi_initialize")) {
+    library("Rmpi")
+    }
+
+print(mpi.universe.size())
+ns <- mpi.universe.size() - 1
+mpi.spawn.Rslaves(nslaves=ns)
 
 print("Setting simulation variables")
 
-candidates=10
+candidates=4
 ballots = 10000
-unfair.ballots = c('.10.9.8.7.6.5.4.3.2','.1.2.3.4.5.6.7.8.9')
+unfair.ballots = c('.4.3.2','.1.2.3')
 out.close = 'close_election_10.txt'
 out.uniform = 'uniform_election_10.txt'
 
 print("Initializing dirichlet trees")
 
-# We generate two elections with 1000 ballots, one which is close between
-# candidates 1 and 2,
-close.irv <- dirtree(
-    type='irv',
-    candidates=candidates,
-    ptype='constant',
-    scale=1
-)
-# and the other which in which ballots are entirely uniform
-uniform.irv <- dirtree(
-    type='irv',
-    candidates=candidates
-)
+simulate <- function(){
+    root <- dirtree( type='irv', candidates=candidates )
+    if(mpi.comm.rank()==0){
+        outfile <- out.close
+        dirtree.update(tree=root,unfair.ballots)
+    } else {
+        outfile <- out.uniform
+    }
+    write_lines(
+        dirtree.simulate(
+            node=root,
+            n=ballots
+        ),
+        outfile
+    )        
+}
 
-# we generate the first by updating with two ballots, and then sampling
-print("Updating close election tree with unfair ballots")
-dirtree.update(
-    tree=close.irv,
-    unfair.ballots
-)
-
-print("Simulating close election")
-write_lines(
-    dirtree.simulate(
-        node=close.irv,
-        n=ballots
-    ),
-    out.close
-)
-
-# Then we generate the second election just by sampling the ballots directly
-print("Simulating uniform election")
-write_lines(
-    dirtree.simulate(
-        node=uniform.irv,
-        n=ballots
-    ),
-    out.uniform
-)
