bd4459f1b43f1b0bd76f707cfe52405ec4c6f36b
Floyd Everest <me@floydeverest.com>
2021-05-18 17:34:52 +1000

    some minor performance boosts based on data formatting and IO

--- R/dirtree.simulate.R
+++ R/dirtree.simulate.R
@@ -2,17 +2,25 @@
 # Recursively splits the remaining samples according to a dirichlet multinomial
 # sample at each node.
 dirtree.simulate <- function(
-    node, # Node to simulate from. This should be 'root' if you want to sample 
-          # across all possible ballots.
-    n    # Number of ballots to simulate
+    node,           # Node to simulate from. This should be 'root'
+                    # if you want to sample across all possible ballots.
+    n,              # Number of ballots to simulate
+    format='counts' # Indicates whether unique ballots have a ':count'
+                    # appended, or if it is a list with duplicated ballots.
+                    # Can be 'counts' or 'duplicated'.
 ){
     # Requires the extraDistr package for dirichlet-multinomial sampling
     if(!require('extraDistr')){
         stop("Sampling requires the `extraDistr` package!")
     }
-    # terminate at leaf nodes
-    if(length(node$children)==0){
-        return(rep(node$name,n))
+    # terminate at leaf nodes or when n is 0
+    if(n==0 || length(node$children)==0){
+        if (format=='counts' && n!=0){
+            return(paste(node:name,':',n))
+        } else if (format=='duplicated') {
+            return(rep(node$name,n))
+        }
+        return('')
     }
 
     # get alphas of the children
@@ -23,7 +31,7 @@ dirtree.simulate <- function(
     # sample at each child node, corresponding to the number of samples which
     # continue down that subtree
     results <- c()
-    
+
     for(i in 1:length(node$children)){
         child.result <- dirtree.simulate(n = dat[i], node = node$children[[i]])
         results <- c(results, child.result)
