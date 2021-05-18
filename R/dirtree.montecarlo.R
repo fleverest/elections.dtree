1a6ae18633a4607d9de1139adc48b1ef0446d40a
Floyd Everest <me@floydeverest.com>
2021-05-18 20:28:30 +1000

    Added a new batchjobs example

--- R/dirtree.montecarlo.R
+++ R/dirtree.montecarlo.R
@@ -13,13 +13,11 @@ dirtree.montecarlo <- function(
     return(outcomes)
 }
 
-# In this function I apply an analytical solution (potentially inefficient) to 
-# find the probability that a candidate receives the least next-preference votes
-dirtree.montecarlo.analytical <- function(
+dirtree.montecarlo.parallel <- function(
     tree, # Election tree to complete
     n, # Number of ballots left
-    m, # Number of elections to simulate
-    eliminated # Candidates which have been eliminated
+    m # Number of elections to simulate
 ){
+   # WIP
 
 }
