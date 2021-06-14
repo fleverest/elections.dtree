d28718a3e7f1ca67da7185d34aae725c60f11c2e
Floyd Everest <me@floydeverest.com>
2021-06-15 04:04:30 +1000

    Fixed some distribution bugs

--- src/distributions.h
+++ src/distributions.h
@@ -24,16 +24,14 @@ int **rDirichletMultinomial(int n,              // Number of repetitions.
                       // repetition. Note the dimension-first index.
   int *gamma_sums =
       new int[n]; // Array of sum of gamma variates for each repetition.
-  float sum_ps, p, sum_alpha, a, gam;
+  float sum_ps, p, a, gam;
   int remaining, count;
 
   // Sample gamma variates for each alpha. Additionally we calculate a sum of
   // the alpha parameters here.
-  sum_alpha = 0.0;
   for (int i = 0; i < d; ++i) {
     gammas[i] = new float[n];
     a = alpha[i]; // Single alpha float (category i+1) for gamma parameter.
-    sum_alpha += a;
     std::gamma_distribution<float> g(a, 1.0);
     // Sample the gamma variate for category i+1 for the jth repetition.
     for (int j = 0; j < n; ++j) {
@@ -71,22 +69,25 @@ int **rDirichletMultinomial(int n,              // Number of repetitions.
  * sampled ballots which are prepended with the first nCompleted choices of
  * indexArray.
  */
-election *rElections(int *nBallots, int nElections, int nCandidates,
-                     int *indexArray, int *permutationArray, int nChosen,
-                     std::mt19937 engine) {
+election *rElections(float scale, int *nBallots, int nElections,
+                     int nCandidates, int *indexArray, int *permutationArray,
+                     int nChosen, std::mt19937 engine, bool isVanilla,
+                     int *factorials) {
   BallotCount *bc;
   bool atLeastOne;
   int *nextNBallots;
   int **countsForChildren;
   election *childBallotSets;
   election *out = new election[nElections];
-  float *alpha =
-      new float[nCandidates]; // Initalize alpha to uniform parameter of ones.
-  for (int i = 0; i < nChosen - nCandidates; ++i) {
-    alpha[i] = 1.;
+  float *alpha = new float[nCandidates - nChosen];
+  // Initalize alpha to uniform parameter of ones.
+  for (int i = 0; i < nCandidates - nChosen; ++i) {
+    alpha[i] = scale;
+    if (isVanilla)
+      alpha[i] = alpha[i] * factorials[nCandidates - nChosen];
   }
-  if (nChosen ==
-      nCandidates - 1) { // If ballot is completely specified, return.
+  if (nChosen == nCandidates - 1) {
+    // If ballot is completely specified, return.
     for (int i = 0; i < nElections; ++i) {
       if (nBallots[i] == 0)
         continue;
@@ -111,8 +112,8 @@ election *rElections(int *nBallots, int nElections, int nCandidates,
   for (int i = 0; i < nCandidates - nChosen; ++i) {
     nextNBallots = new int[nElections];
     atLeastOne = 0;
-    for (int j = 0; j < nElections;
-         ++j) { // construct next nBallots array for samples.
+    // construct next nBallots array for samples.
+    for (int j = 0; j < nElections; ++j) {
       nextNBallots[j] = countsForChildren[j][i];
       atLeastOne = (atLeastOne || nextNBallots[j]);
     }
@@ -123,9 +124,9 @@ election *rElections(int *nBallots, int nElections, int nCandidates,
     // Get candidate number of corresponding index.
     indexArray[nChosen] = i;
     std::swap(permutationArray[i + nChosen], permutationArray[nChosen]);
-    childBallotSets =
-        rElections(nextNBallots, nElections, nCandidates, indexArray,
-                   permutationArray, nChosen + 1, engine);
+    childBallotSets = rElections(scale, nextNBallots, nElections, nCandidates,
+                                 indexArray, permutationArray, nChosen + 1,
+                                 engine, isVanilla, factorials);
     std::swap(permutationArray[i + nChosen], permutationArray[nChosen]);
     for (int j = 0; j < nElections; ++j) {
       out[j].insert(out[j].end(),
