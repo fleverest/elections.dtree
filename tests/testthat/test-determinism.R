c4ee7eded13ec4d5ca524d1738e71cc661311179
Floyd Everest <me@floydeverest.com>
2022-05-11 19:58:34 +1000

    added c++ tests

---
+++ tests/testthat/test-determinism.R
@@ -0,0 +1,37 @@
+
+seed <- runif(1, 0, 1000)
+
+dtree <- dirtree.pirv(candidates=LETTERS[1:10], alpha0=1., minDepth=0)
+
+test_that("`samplePredictive` is deterministic with specified seed", {
+  set.seed(seed)
+  bs.1 <- samplePredictive(dtree, 1000)
+  set.seed(seed)
+  bs.2 <- samplePredictive(dtree, 1000)
+  expect_true(
+    identical(bs.1, bs.2),
+    "`samplePredictive` is not deterministic with constant seed."
+  )
+})
+
+test_that("`samplePosterior` (nElections=1) is deterministic with specified seed", {
+  set.seed(seed)
+  ps.1 <- samplePosterior(dtree, 1, 1000)
+  set.seed(seed)
+  ps.2 <- samplePosterior(dtree, 1, 1000)
+  expect_true(
+    identical(ps.1, ps.2),
+    "`samplePosterior` is not deterministic with constant seed when nElections=1."
+  )
+})
+
+test_that("`samplePosterior` (nElections=100) is deterministic with specified seed", {
+  set.seed(seed)
+  ps.1 <- samplePosterior(dtree, 100, 1000)
+  set.seed(seed)
+  ps.2 <- samplePosterior(dtree, 100, 1000)
+  expect_true(
+    identical(ps.1, ps.2),
+    "`samplePosterior` is not deterministic with constant seed when nElections=100."
+  )
+})
