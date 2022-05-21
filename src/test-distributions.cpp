ab6bb244c685d328edc900d66f21d70570ced87d
Floyd Everest <me@floydeverest.com>
2022-05-21 19:58:31 +1000

    fixed a minor numerical instability where the dirichlet-multinomial distribution did not always sum to count (it was usually count-1 in with high-dimensional a parameter).

--- src/test-distributions.cpp
+++ src/test-distributions.cpp
@@ -20,25 +20,29 @@ context("Test Dirichlet-Multinomial samples sum to count.") {
 
   bool always_sums_to_count = true;
 
-  for (unsigned d = 1; d < 1000; ++d) {
-    // Initialize a new a vector.
-    a = new float[d];
-    for (unsigned i = 0; i < d; ++i)
-      a[i] = g(mte);
+  for (auto z = 0; z < 10; ++z) {
+    for (unsigned d = 1; d < 1000; ++d) {
+      // Initialize a new a vector.
+      a = new float[d];
+      for (unsigned i = 0; i < d; ++i)
+        a[i] = g(mte);
 
-    result = rDirichletMultinomial(count, a, d, &mte);
+      result = rDirichletMultinomial(count, a, d, &mte);
 
-    // Sum the result
-    sum = 0;
-    for (unsigned i = 0; i < d; ++i)
-      sum += result[i];
+      // Sum the result
+      sum = 0;
+      for (unsigned i = 0; i < d; ++i)
+        sum += result[i];
 
-    if (sum != count)
-      always_sums_to_count = false;
+      if (sum != count) {
+        always_sums_to_count = false;
+        break;
+      }
 
-    // Delete a and mnom counts
-    delete[] a;
-    delete[] result;
+      // Delete a and mnom counts
+      delete[] a;
+      delete[] result;
+    }
   }
   test_that("Dirichlet-Multinomial sample sums to count.") {
     expect_true(always_sums_to_count);
